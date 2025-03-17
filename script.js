2e2e00a330e939aae93aad3b141eca623b0bc0fd
safal-t <safaltimalsena0@gmail.com>
2025-03-18 08:41:24 +1100

    GUI complete

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
+// DEFINE VARIABLES 
 const gridContainer = document.querySelector(".grid-container")
+const setSquaresBtn = document.querySelector(".set-squares-btn")
 
-const numOfSquares = 16; 
+let numOfRows = 16;
 
-const createGrid = (numOfSquares) => {
-    for (let i = 0; i < numOfSquares; i++) {
-        const gridBox = document.createElement("div");
-        gridBox.classList.add("grid-box");
-        gridContainer.appendChild(gridBox);
+
+
+// DEFINE FUNCTIONS
+const createGrid = (numOfRows) => {
+    for (let i = 0; i < numOfRows; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        for (let j = 0; j < numOfRows; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            gridRow.appendChild(gridSquare);
+        }   
+        gridContainer.appendChild(gridRow);
     }
+    const gridSquares = document.querySelectorAll(".grid-square")
+    onHover(gridSquares)
 }
 
-createGrid(numOfSquares)
+
+const onHover = (allSquares) => {
+    allSquares.forEach((square) => {
+        square.addEventListener('mouseover', () => square.classList.add('hovered-square'))
+    })
+}
+
+const changeGrid = () => {
+    numOfRows = prompt("How many squares would you like? Max = 100")
+    if (numOfRows > 100) {
+        alert("Number of squares cannot be greater than 100")
+    }
+    clearGrid()
+    createGrid(numOfRows)
+}
+
+const clearGrid = () => {
+    gridContainer.innerHTML = ''
+}
+
+// CALL FUNCTIONS
+createGrid(numOfRows)
+
+// ADD EVENT LISTENERS
+setSquaresBtn.addEventListener('click', () => changeGrid())
